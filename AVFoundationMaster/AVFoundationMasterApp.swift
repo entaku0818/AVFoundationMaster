05d73a0c2f4f4815eebd005bcc802de10f650644
entaku <entaku19890818@gmail.com>
2024-05-11 21:51:45 +0900

    add

---
+++ AVFoundationMaster/AVFoundationMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVFoundationMasterApp.swift
+//  AVFoundationMaster
+//
+//  Created by 遠藤拓弥 on 2024/05/11.
+//
+
+import SwiftUI
+
+@main
+struct AVFoundationMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AVFoundationMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AVFoundationMaster
+//
+//  Created by 遠藤拓弥 on 2024/05/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
